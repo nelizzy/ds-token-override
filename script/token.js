80c562ae1462d866c328fe8c36376f8741b059cb
nelizzy <14821749+nelizzy@users.noreply.github.com>
2026-05-15 15:25:04 +0200

    Add minion healthbar fix

--- script/token.js
+++ script/token.js
@@ -1,9 +1,10 @@
-import { settings } from "./utils";
+import { mod as mod, settings } from "./utils.js";
 
 export const init = async () => {
   tokenResource.init();
   hudRolls.init();
   healthLabels.init();
+  minionHealthbarFix();
 }
 
 /* ---------------------------- RELEVANT SETTINGS ---------------------------
@@ -13,6 +14,7 @@ export const init = async () => {
 - enableTokenResource (boolean)
 - tokenResourceSize (number)
 
+- healthbarTicks (boolean)
 - healthLabelPlayersMinimumPerm (string: CONST.USER_ROLE_NAMES)
 - healthLabelOtherMinimumPerm (string: CONST.USER_ROLE_NAMES)
 - healthLabelSize (number)
@@ -21,16 +23,46 @@ export const init = async () => {
 // Might refactor to split these three out further? :thonk:
 
 export const tokenResource = (() => {
+  const init = () => { };
 
   return { init };
 })();
 
 export const hudRolls = (() => {
+  const init = () => { };
 
   return { init };
 })();
 
 export const healthLabels = (() => {
+  const init = () => { };
 
   return { init };
 })();
+
+
+// delete when fixed in base foundry!!
+const minionHealthbarFix = () => {
+  // fix initial load
+  Hooks.on("drawTokenLayer", (tokenLayer) => {
+    tokenLayer.ownedTokens.forEach(token => {
+      if (token?.actor?.system?.combatGroups?.size === 1) {
+        token.document._prepareBars();
+        token.animate(token._getAnimationData(), { duration: 0 });
+      }
+    })
+  });
+
+  // fix health updates
+  (() => {
+    const old = CONFIG.Combatant.documentClass.prototype.refreshCombatant;
+    CONFIG.Combatant.documentClass.prototype.refreshCombatant = function () {
+      if (this.actor.system.combatGroups.size === 1) {
+        this.token?._prepareBars();
+        this.token?.object?.animate(this.token?.object?._getAnimationData());
+      }
+
+      old.call(this);
+    }
+  })();
+}
