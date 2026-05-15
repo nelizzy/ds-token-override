9787b26e43bcaac8e591295a12c38a17209f03b3
nelizzy <14821749+nelizzy@users.noreply.github.com>
2026-05-15 15:25:57 +0200

    Add debug fn and extra inits

--- script/index.js
+++ script/index.js
@@ -1,7 +1,10 @@
-import config from "./config";
-import quickRoll from "./quickRoll";
-import damageLog from "./damageLog";
-import combatTracker from "./combatTracker";
+import * as config from "./config.js";
+import * as quickRoll from "./quickRoll.js";
+import * as damageLog from "./damageLog.js";
+import * as combatTracker from "./combatTracker.js";
+import * as token from "./token.js";
+import { mod } from "./utils.js";
+import { socket } from "./socket.js";
 
 Hooks.once("init", () => {
   // initialize settings
@@ -11,4 +14,11 @@ Hooks.once("init", () => {
   quickRoll.init();
   damageLog.init();
   combatTracker.init();
+  token.init();
+
+  // initialize sockets
+  socket.init();
 });
+
+window.toggleHooks = () => CONFIG.debug.hooks = !CONFIG.debug.hooks;
+// toggleHooks();
