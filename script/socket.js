864352f7ed17cd52fa3b06e440852df5ae47fd22
nelizzy <14821749+nelizzy@users.noreply.github.com>
2026-05-15 15:24:11 +0200

    Implement socket access

--- script/socket.js
+++ script/socket.js
@@ -1,35 +1,30 @@
-import { flags, modConsole, moduleId } from "./utils";
+import { MODULE_ID } from "./const.js";
+import { mod } from "./utils.js";
 
 export const socket = (() => {
-  const id = `module.${moduleId}`;
-
   const emit = (action, data) => {
-    if (actions.get(action)) {
-      game.socket.emit(id, { action, ...data })
+    if (list.get(action)) {
+      game.socket.emit(`module.${MODULE_ID}`, { action, ...data })
     } else {
-      modConsole.warn(`No socket handler for action ${action}`);
+      mod.warn(`SOCKET: No action has been registered for ${action}`);
     }
   };
 
-  const actions = new Map();
+  const list = new Map();
 
   const register = (action, callback) => {
-    actions.set(action, callback);
+    mod.log(`SOCKET: Registered ${action}`)
+    list.set(action, callback);
   };
 
   const init = () => {
-    game.socket.on(id, async ({ action, ...data }) => {
-      const callback = actions.get(action);
+    game.socket.on(`module.${MODULE_ID}`, async (payload) => {
+      const { action, ...data } = payload;
+      const callback = list.get(action);
       if (callback) await callback(data);
+      else mod.warn(`SOCKET: Could not run ${action}`)
     });
   }
 
-  return { emit, register, actions, init }
+  return { emit, register, list, init }
 })();
-
-socket.register("setUndone", async ({ messageId }) => {
-  if (!game.user.isGM) return;
-  const message = await game.messages.get(messageId);
-  await flags(message).set("undone", true);
-});
-
